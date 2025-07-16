f7fb4a2ed8d32cb4b63d554aa709f63c33bc65b3
Felix Diaz <fld1@williams.edu>
2025-07-16 15:23:32 -0400

    updating repo

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#SBATCH --job-name=wind                   # job name
-#SBATCH --partition=hpc                   # partition or queue name
-#SBATCH --nodes=1                         # num of nodes
-#SBATCH --ntasks-per-node=1               # num of tasks per node
-#SBATCH --cpus-per-task=50                # num of CPU cores per task
-#SBATCH --mem-per-cpu=1G                  # memory per CPU requested
-#SBATCH --time=00:30:00                   # max runtime
-#SBATCH --output=job_output_%j.out        # job output
-#SBATCH --error=job_output_%j.err         # job error output
-#SBATCH --mail-type=START,END,FAIL        # send email at start and end of job
-#SBATCH --mail-user=fld1@williams.edu     # email address for notifications
+#SBATCH --job-name=wind
+#SBATCH --partition=hpc
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=50
+#SBATCH --mem-per-cpu=1G
+#SBATCH --time=00:30:00
+#SBATCH --output=job_output_%j.out
+#SBATCH --error=job_output_%j.err
+#SBATCH --mail-type=START,END,FAIL
+#SBATCH --mail-user=fld1@williams.edu
 
 # load python interpreter
 source ~/miniconda3/etc/profile.d/conda.sh
